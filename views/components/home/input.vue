08e7574e3c25d179ae4636fddb73346a1db9a243
Noel RIGNON <noel.rignon@fjnr.ca>
2018-09-04 16:39:02 -0400

    use iframe in place of redirection

--- views/components/home/input.vue
+++ views/components/home/input.vue
@@ -1,8 +1,19 @@
 <template>
-  <form class="homepage__form" @submit.prevent="sendWebsiteLink">
-    <input class="homepage__input" type="search" v-model="website" placeholder="votresite.com" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" />
-    <span class="homepage__help">Appuyez sur la touche entrer pour voir votre site</span>
-  </form>
+  <div class="homepage">
+    <form v-if="show" class="homepage__form" @submit.prevent="sendWebsiteLink">
+      <span v-if="redirected_url" class="homepage__form__cancel" @click.prevent="toggleInput" >X</span>
+      <input class="form__input" type="search" v-model="website" placeholder="votresite.com" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" />
+      <span class="form__help">Appuyez sur la touche entrer pour voir votre site</span>
+      <div>
+        <input type="checkbox" v-model="iframe_supported" id="redirection" /> <label for="redirection">Votre site accepte t-il les iframes ?</label>
+      </div>
+      <button type="submit" class="form__button">Tester</button>
+    </form>
+    <a v-if="!show" @click.prevent="toggleInput" class="homepage__arrow">
+      >
+    </a>
+    <iframe class="homepage__iframe" v-if="redirected_url" v-bind:src="redirected_url"></iframe>
+  </div>
 </template>
 
 <script>
@@ -11,6 +22,9 @@ const io = require('socket.io-client');
 export default {
     data() {
         return {
+            show: true,
+            redirected_url: '',
+            iframe_supported: true,
             website: '',
             socket: io('localhost:8080')
         }
@@ -18,7 +32,8 @@ export default {
     methods: {
         sendWebsiteLink() {
             this.socket.emit('SEND_WEBSITE', {
-                website: this.addhttp(this.website)
+                website: this.addhttp(this.website),
+                redirection: !this.iframe_supported
             });
             this.website = '';
         },
@@ -27,11 +42,19 @@ export default {
                url = "http://" + url;
             }
             return url;
+        },
+        toggleInput() {
+            this.show = !this.show;
         }
     },
     mounted() {
         this.socket.on('REDIRECT', (data) => {
-            window.location.replace(data.website);
+            if (data.redirection) {
+              window.location.replace(data.website);
+            } else {
+              this.redirected_url = data.website;
+              this.show = false;
+            }
         });
     }
 };
--- views/pages/home/index.vue
+++ views/pages/home/index.vue
@@ -1,27 +1,13 @@
 <template>
-    <div class="homepage container container--no-padding">
-        <div class="homepage__inner homepage__inner--up grid">
-            <inputComponent></inputComponent>
-        </div>
-        <div class="homepage__inner homepage__inner--down">
-            <div class="homepage__more">
-                <salesComponent></salesComponent>
-                <infosComponent></infosComponent>
-            </div>
-        </div>
-    </div>
+    <inputComponent></inputComponent>
 </template>
 
 <script>
 const inputComponent = require('../../components/home/input.vue').default;
-const salesComponent = require('../../components/home/sales.vue').default;
-const infosComponent = require('../../components/home/infos.vue').default;
 
 export default {
     components: {
-        inputComponent,
-        salesComponent,
-        infosComponent
+        inputComponent
     }
 };
 </script>
